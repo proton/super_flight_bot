cba0b805dee3026b8be1a9ddc6ecd9fb82b212f8
Peter Savichev (proton) <psavichev@gmail.com>
2018-07-05 21:10:06 +0200

    a lot of changes

--- bot.js
+++ bot.js
@@ -1,14 +1,43 @@
 const TeleBot = require('telebot');
 const bot = new TeleBot(process.env.TG_TOKEN);
 const vkapi = new (require('node-vkapi'))({ accessToken: process.env.VK_TOKEN });
+const Datastore = require('nedb')
+let db = new Datastore({ filename: 'var/data.db', autoload: true });
 const LOOP_INTERVAL = 3000;
 
-let usersKeywords = {
-  60037421: ['берлин']
+let keywordUsers = {
+  'берлин': [60037421]
 }
 
 const vkGroups = [63731512, 119000633];
 let vkGroupLastMessageIds = {};
+
+function postUrl(post) {
+  return `https://vk.com/wall${post.fromId}_${post.id}`
+}
+
+function postToMessage(post) {
+  return post.text + "\n\n" + postUrl(post);
+}
+
+function sendPostToUsers(bot, post) {
+  let sentToUsers = {};
+  let lowercased_text = post.text.toLowerCase();
+  
+  for (let keyword in keywordUsers) {
+    //TODO: guard
+    if (lowercased_text.includes(keyword)) {
+      let text = postToMessage(post);
+      userIds = keywordUsers[keyword];
+      for (let userId in userIds) {
+        if (!sentToUsers[userId]) {
+          sentToUsers[userId] = true;
+          bot.sendMessage(userId, text);
+        }
+      }
+    }
+  }
+}
 
 function loadGroupPosts(groupId) {
   vkapi.call('wall.get', { owner_id: -groupId, count: 100 })
@@ -16,21 +45,10 @@ function loadGroupPosts(groupId) {
       response.items.reverse().forEach(post => {
         if (vkGroupLastMessageIds[groupId] > post.id) return;
         vkGroupLastMessageIds[groupId] = post.id;
-
-        let lowercased_text = post.text.toLowerCase();
-        const url = `https://vk.com/wall-${groupId}_${post.id}`
-        for (var user_id in usersKeywords) {
-          let keywords = usersKeywords[user_id];
-          if(keywords.some(keyword => lowercased_text.includes(keyword))) {
-            let text = post.text + "\n\n" + url;
-            bot.sendMessage(user_id, text);
-          }
-        }
+        sendPostToUsers(bot, post);
       });
     });
 }
-
-bot.start();
 
 function loadNewPosts() {
   vkGroups.forEach(vkGroupId => loadGroupPosts(vkGroupId));
@@ -41,18 +59,50 @@ function loadNewPostsLoop() {
   setTimeout(loadNewPostsLoop, LOOP_INTERVAL);
 }
 
-bot.on('/keywords', (msg) => {
+function userKeywordsMessage(userId) {
   const keywords = usersKeywords[msg.from.id];
-  if (keywords.length) answer = 'Keywords:\n' + keywords.join('\n')
-  else answer = 'No keywords\nEnter\n/add keyword\nto add new one'
+  if (keywords.length) return 'Your keywords:\n' + keywords.join('\n')
+  return 'No keywords\nEnter\n/add keyword\nto add new one'
+}
+
+bot.on('/keywords', (msg) => {
+  let answer = userKeywordsMessage(msg.from.id);
   return msg.reply.text(answer);
 })
 
 bot.on(/^\/add (.+)$/, (msg, props) => {
-  const newKeyword = props.match[1];
-  usersKeywords[msg.from.id].push(newKeyword);
-  let answer = `Added keyword ${newKeyword}`
+  const keyword = props.match[1];
+  usersKeywords[msg.from.id].push(keyword);
+  let answer = `Added keyword ${keyword}`
+    + "\n\n" + userKeywordsMessage(msg.from.id);
   return msg.reply.text(answer);
 });
 
+bot.on(/^\/delete (.+)$/, (msg, props) => {
+  const keyword = props.match[1];
+  // usersKeywords[msg.from.id].remove(keyword); ???
+  let answer = `Deleted keyword ${keyword}`
+    + "\n\n" + userKeywordsMessage(msg.from.id);
+  return msg.reply.text(answer);
+});
+
+function helpMessage() {
+  return '' +
+    '/add keyword - Adds new keyword to the list\n' +
+    '/delete keyword - Delete keyword from the list\n' +
+    '/keywords - List your keywords\n' +
+    '/help - This help';
+}
+
+bot.on('/start', (msg) => {
+  let answer = helpMessage();
+  return msg.reply.text(answer);
+});
+
+bot.on('/help', (msg) => {
+  let answer = helpMessage();
+  return msg.reply.text(answer);
+});
+
+bot.start();
 loadNewPostsLoop();
